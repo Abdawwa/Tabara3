72ac4c5d6bfeb473f9426d664b9fd43fabc345d5
tarequi <57463027+tarequi@users.noreply.github.com>
2022-04-17 00:16:51 +0300

    initial commit

---
+++ login.php
@@ -0,0 +1,93 @@
+<?php
+require_once "config.php";
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Login</title>
+    <style>
+        body {
+            background-color: rgb(127 188 255);
+        }
+
+        .padding {
+            padding: 5rem !important
+        }
+
+        .signup-form {
+            background-color: #fff;
+            padding: 45px;
+            border-radius: 25px;
+            width: 500px;
+            margin: auto;
+        }
+
+        h2 {
+            font-size: 36px;
+            letter-spacing: 0.08em
+        }
+
+        .signup-form .form-control {
+            font-size: 16px;
+            padding: 10px 15px;
+            color: #555;
+            background-color: #fff;
+            border-radius: 3px
+        }
+
+        .signup-form input {
+            border: 1px solid #eee;
+            height: 38px;
+            box-shadow: none !important
+        }
+
+        .btn-blue {
+            background: #44c5ee;
+            padding: 10px 28px;
+            border: 2px solid #44c5ee;
+            color: #fff;
+            border-radius: 50px;
+            font-weight: 700;
+            letter-spacing: 0.08em;
+            -webkit-transition: 0.5s all;
+            transition: 0.5s all;
+            box-shadow: 0px 0px 60px 0px rgba(68, 197, 238, 0.6);
+            outline: none !important
+        }
+
+        .btn-blue:hover,
+        .btn-blue:focus,
+        .btn-blue:active {
+            background: #fff;
+            color: #496174
+        }
+    </style>
+</head>
+
+<body>
+    <div class="padding container d-flex justify-content-center">
+        <div class="col-md-10 col-md-offset-1">
+            <form class="signup-form" enctype="multipart/form-data" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <h2 class="text-center">Login</h2>
+                <hr>
+                <div class="form-group"> <input type="text" name="Aemail" class="form-control" placeholder="Email Address"> </div>
+                <div class="form-group"> <input type="password" name="Apass" class="form-control" placeholder="Password"> </div>
+                <div class="form-group text-center"> <button type="submit" name="submit" class="btn btn-blue btn-block">Start Now</button> </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
