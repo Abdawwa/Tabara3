be363c10826c46b24409c15e01d8eb5dd6a92e6b
unknown <abdallah.awa99@gmail.com>
2022-05-06 21:46:02 +0300

    fixed signup message for empty fields

--- login.php
+++ login.php
@@ -1,13 +1,7 @@
 <?php
 require_once "config.php";
 require_once "functions/validate.php";
-include "header.php"
-?>
-
-
-
-<?php
-
+include "header.php";
 if (isset($_POST['submit'])) {
     $userEmail = trim($_POST['userEmail']);
     $userPass = trim($_POST['userPass']);
@@ -37,98 +31,102 @@ if (isset($_POST['submit'])) {
             $error_message = "Email Not Valid";
         }
     } else {
-        $error_message = "Please fill All Filds";
+        $error_message = "Please fill All Fields";
     }
     require_once "functions/messagees.php";
 }
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-    <title>Login</title>
-    <style>
-        body {
-            background-color: rgb(127 188 255);
-        }
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+        <title>Login</title>
+        <style>
+            body {
+                background-color: rgb(127 188 255);
+            }
 
-        .padding {
-            padding: 5rem !important
-        }
+            .padding {
+                padding: 5rem !important
+            }
 
-        .signup-form {
-            background-color: #fff;
-            padding: 45px;
-            border-radius: 25px;
-            width: 500px;
-            margin: auto;
-        }
+            .signup-form {
+                background-color: #fff;
+                padding: 45px;
+                border-radius: 25px;
+                width: 500px;
+                margin: auto;
+            }
 
-        h2 {
-            font-size: 36px;
-            letter-spacing: 0.08em
-        }
+            h2 {
+                font-size: 36px;
+                letter-spacing: 0.08em
+            }
 
-        .signup-form .form-control {
-            font-size: 16px;
-            padding: 10px 15px;
-            color: #555;
-            background-color: #fff;
-            border-radius: 3px
-        }
+            .signup-form .form-control {
+                font-size: 16px;
+                padding: 10px 15px;
+                color: #555;
+                background-color: #fff;
+                border-radius: 3px
+            }
 
-        .signup-form input {
-            border: 1px solid #eee;
-            height: 38px;
-            box-shadow: none !important
-        }
+            .signup-form input {
+                border: 1px solid #eee;
+                height: 38px;
+                box-shadow: none !important
+            }
 
-        .btn-blue {
-            background: #28a745;
-            padding: 10px 28px;
-            border: 2px solid #28a745;
-            color: #fff;
-            border-radius: 50px;
-            font-weight: 700;
-            letter-spacing: 0.08em;
-            -webkit-transition: 0.5s all;
-            transition: 0.5s all;
-            outline: none !important
-        }
+            .btn-blue {
+                background: #28a745;
+                padding: 10px 28px;
+                border: 2px solid #28a745;
+                color: #fff;
+                border-radius: 50px;
+                font-weight: 700;
+                letter-spacing: 0.08em;
+                -webkit-transition: 0.5s all;
+                transition: 0.5s all;
+                outline: none !important
+            }
 
-        .btn-blue:hover,
-        .btn-blue:focus,
-        .btn-blue:active {
-            background: #fff;
-            color: #496174
-        }
-    </style>
-</head>
+            .btn-blue:hover,
+            .btn-blue:focus,
+            .btn-blue:active {
+                background: #fff;
+                color: #496174
+            }
+        </style>
+    </head>
 
-<body>
+    <body>
     <div class="padding container d-flex justify-content-center">
         <div class="col-md-10 col-md-offset-1">
-            <form class="signup-form" enctype="multipart/form-data" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+            <form class="signup-form" enctype="multipart/form-data" method="POST"
+                  action="<?php echo $_SERVER['PHP_SELF']; ?>">
                 <h2 class="text-center">Login</h2>
-                <hr>
-                <div class="form-group"> <input type="text" name="userEmail" class="form-control" placeholder="Email Address"> </div>
-                <div class="form-group"> <input type="password" name="userPass" class="form-control" placeholder="Password"> </div>
-                <div class="form-group text-center"> <button type="submit" name="submit" class="btn btn-blue btn-block">Start Now</button> </div>
+                <div class="form-group"><input type="text" name="userEmail" class="form-control"
+                                               placeholder="Email Address"></div>
+                <div class="form-group"><input type="password" name="userPass" class="form-control"
+                                               placeholder="Password"></div>
+                <div class="form-group text-center">
+                    <button type="submit" name="submit" class="btn btn-blue btn-block">Start Now</button>
+                </div>
             </form>
         </div>
     </div>
-</body>
+    </body>
 
-</html>
+    </html>
 <?php
 include "footer.php";
 ?>
--- signup.php
+++ signup.php
@@ -2,15 +2,13 @@
 require_once "config.php";
 require_once "functions/validate.php";
 include "header.php";
-?>
-
-<?php
-if (isset($_POST['submit']) && isset($_POST['gender'])) {
+if (isset($_POST['submit'])) {
     $userName = trim($_POST['userName']);
     $userEmail = trim($_POST['userEmail']);
     $userPass = trim($_POST['userPassword']);
     $userPhone = trim($_POST['userPhone']);
-    $userGender = $_POST['gender'];
+    if (isset($_POST['gender']))
+        $userGender = $_POST['gender'];
 
 
     if (ChickEmpty($userName) && ChickEmpty($userEmail) && ChickEmpty($userPass) && ChickEmpty($userPhone) && ChickEmpty($userGender)) {
@@ -32,130 +30,135 @@ if (isset($_POST['submit']) && isset($_POST['gender'])) {
             $error_message = "Email Not Valid!!";
         }
     } else {
-        $error_message = "Please fill All Filds";
+        $error_message = "Please fill All Fields";
     }
     //require_once 'functions/messagees.php';
 }
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-    <title>sign up</title>
-    <style>
-        .form {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            border: solid #5CDB95 2px;
-            border-radius: 25px;
-            padding: 25px 0;
-            width: 400px;
-            max-width: 100%;
-            margin: auto;
-
-        }
-
-        .form-group {
-            font-size: 20px;
-            font-weight: 400;
-        }
-
-        .gender {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-
-        .sub-btn {
-            float: right;
-            border: none;
-            padding: 10px 20px;
-            background-color: #5CDB95;
-            color: white;
-            border-radius: 25px;
-        }
-
-        form {
-            width: 250px;
-        }
-
-        @media (max-width: 575px) {
-            .form {
-                padding: 0;
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+        <title>sign up</title>
+        <style>
+            body {
+                background-color: rgb(127 188 255);
             }
-        }
 
-        .footer {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-        }
-    </style>
-</head>
+            .form {
+                position: relative;
+                left: 50%;
+                transform: translateX(-50%);
+                border-radius: 25px;
+                padding: 25px 0;
+                width: 400px;
+                max-width: 100%;
+                background-color: white;
+                margin: 30px 0px;
+            }
 
-<body>
-<div class="signup">
-    <div class="container">
-        <div class="row col-md-6 d-flex justify-content-center form">
-            <h2>Registration</h2>
-            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
-                <div class="form-group ">
-                    <label for="name">Username</label>
-                    <input type="text" class="form-control" name="userName" id="name" placeholder="Username"
-                           aria-label="Username" aria-describedby="basic-addon1">
-                </div>
-                <div class="form-group ">
-                    <label for="Email">Email</label>
-                    <input type="text" class="form-control" name="userEmail" id="Email" placeholder="Email"
-                           aria-label="Username" aria-describedby="basic-addon1">
-                </div>
-                <div class="form-group ">
-                    <label for="Password">Password</label>
-                    <input type="Password" class="form-control" name="userPassword" id="Password" placeholder="Password"
-                           aria-label="Username" aria-describedby="basic-addon1">
-                </div>
-                <div class="form-group ">
-                    <label for="Phone">Phone</label>
-                    <input type="text" class="form-control" name="userPhone" id="Phone" placeholder="Phone"
-                           aria-label="Username" aria-describedby="basic-addon1">
-                </div>
-                <div class="form-group">
-                    <label>Gender</label><br>
-                    <div class="gender">
-                        <input type="radio" id="Male" name="gender" value="Male">
-                        <label for="Male">Male</label><br>
-                        <input type="radio" id="Female" name="gender" value="Female">
-                        <label for="Female">Female</label><br>
+            .form-group {
+                font-size: 20px;
+                font-weight: 400;
+            }
+
+            .gender {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+            }
+
+            .sub-btn {
+                float: right;
+                border: none;
+                padding: 10px 20px;
+                background-color: #5CDB95;
+                color: white;
+                border-radius: 25px;
+            }
+
+            form {
+                width: 250px;
+            }
+
+            @media (max-width: 575px) {
+                .form {
+                    padding: 0;
+                }
+            }
+
+            .footer {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+            }
+        </style>
+    </head>
+    <body>
+    <div class="signup">
+        <div class="container">
+            <div class="row col-md-6 d-flex justify-content-center form">
+                <h2>Registration</h2>
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+                    <div class="form-group ">
+                        <label for="name">Username</label>
+                        <input type="text" class="form-control" name="userName" id="name" placeholder="Username"
+                               aria-label="Username" aria-describedby="basic-addon1">
                     </div>
-                </div>
-                <div class="footer-msg">
-                    <?php if (isset($success_message) && $success_message != "") { ?>
-                        <p class="alert alert-success text-center"
-                           style="top: 5px; left: -10px;"><?php echo $success_message ?></p>
-                    <?php } ?>
+                    <div class="form-group ">
+                        <label for="Email">Email</label>
+                        <input type="text" class="form-control" name="userEmail" id="Email" placeholder="Email"
+                               aria-label="Username" aria-describedby="basic-addon1">
+                    </div>
+                    <div class="form-group ">
+                        <label for="Password">Password</label>
+                        <input type="Password" class="form-control" name="userPassword" id="Password"
+                               placeholder="Password"
+                               aria-label="Username" aria-describedby="basic-addon1">
+                    </div>
+                    <div class="form-group ">
+                        <label for="Phone">Phone</label>
+                        <input type="text" class="form-control" name="userPhone" id="Phone" placeholder="Phone"
+                               aria-label="Username" aria-describedby="basic-addon1">
+                    </div>
+                    <div class="form-group">
+                        <label>Gender</label><br>
+                        <div class="gender">
+                            <input type="radio" id="Male" name="gender" value="Male">
+                            <label for="Male">Male</label><br>
+                            <input type="radio" id="Female" name="gender" value="Female">
+                            <label for="Female">Female</label><br>
+                        </div>
+                    </div>
+                    <div class="footer-msg">
+                        <?php if (isset($success_message) && $success_message != "") { ?>
+                            <p class="alert alert-success text-center"
+                               style="top: 5px; left: -10px;"><?php echo $success_message ?></p>
+                        <?php } ?>
 
-                    <?php if (isset($error_message) && $error_message != "") { ?>
-                        <p class="alert alert-danger text-center"
-                           style="top: 5px; left: -10px;"><?php echo $error_message ?></p>
-                    <?php } ?>
+                        <?php if (isset($error_message) && $error_message != "") { ?>
+                            <p class="alert alert-danger text-center"
+                               style="top: 5px; left: -10px;"><?php echo $error_message ?></p>
+                        <?php } ?>
 
-                    <button type="submit" class="sub-btn" name="submit">Signup</button>
-                </div>
-            </form>
+                        <button type="submit" class="sub-btn" name="submit">Signup</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
-</body>
 
-</html>
+    </body>
+    </html>
+<?php
+include "footer.php";
+?>
